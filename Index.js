8965ead2992ccb8f9ca87f1cf1574658a03d162a
PunkHazard07 <belloayoola20@gmail.com>
2025-01-16 15:29:23 +0100

    nnnn

--- Index.js
+++ Index.js
@@ -20,6 +20,7 @@ mongoose.connect(dbUrl).then(() => {
     console.log("Database connected");
     const app = express(); //to create an instance of express
     const port =  8080; //to set the port
+    
 
     //Middleware
     app.use(express.json()); //to convert it to json format
