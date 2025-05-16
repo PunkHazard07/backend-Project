04f5cbab559c57a81daa5704983ac920d96636cb
Bello Ayoola Raji <belloayoola20@gmail.com>
2025-05-16 15:20:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ mongoose.connect(dbUrl).then(() => {
         cors: {
             origin: ['http://localhost:5173', 'http://localhost:5174',
                 'http://localhost:5175', 'http://localhost:5176',
-                'http://localhost:5177', 'http://localhost:5178', 'https://creativefurniture.onrender.com'], // Allow both local ports
+                'http://localhost:5177', 'http://localhost:5178', 'https://creativefurniture.onrender.com', 'https://creative-furniture-admin.onrender.com'], // Allow both local ports
             methods: ['GET', 'POST', 'PUT', 'PATCH','DELETE'],
             credentials: true // Add this if you're dealing with cookies or sessions
         }
@@ -66,7 +66,7 @@ mongoose.connect(dbUrl).then(() => {
     app.use(cors({
         origin: ['http://localhost:5173', 'http://localhost:5174',
             'http://localhost:5175', 'http://localhost:5176',
-              'http://localhost:5177', 'http://localhost:5178', 'https://creativefurniture.onrender.com'],  // Allow both local ports
+              'http://localhost:5177', 'http://localhost:5178', 'https://creativefurniture.onrender.com', 'https://creative-furniture-admin.onrender.com'],  // Allow both local ports
         methods: ['GET', 'POST', 'PUT', 'PATCH','DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'Expires', 'Pragma'],
         credentials: true // Add this if you're dealing with cookies or sessions
